54501a062bd736ed6d773df131156330728f7351
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-01-26 19:42:49 -0800

    added const for conditons,housing,location,slogan,reset  and lets for those conditions

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
+import { creatCounterString} from './utils.js';
 // import functions and grab DOM elements
+const conditionsSelect = document.getElementById('conditions-select');
+const housingSelect = document.getElementById('housing-select');
+const locationSelect = document.getElementById('location-select');
+
+const sloganInput = document.getElementById('slogan-input');
+const sloganBtn = document.getElementById('slogan-btn');
+const sloganList = document.getElementById('slogan-list');
+const reset = document.getElementById('reset-slogans');
+
+const results = document.getElmentsById('results');
+
+let slogan = []
+let conditionsCounter = 0;
+let housingCounter = 0;
+let locationCounter = 0;
+
+
+
+
 
 // let state
 
