f497ec552254b56f81b392cf4d5681dbf35513a6
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-01-28 15:23:33 -0800

    add img to css to contain pictures

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,11 +1,1 @@
-import { createCountString } from '../utils.js';
 
-const test = QUnit.test;
-
-test('testing string', (expect) => {
-    const string = ` You picked conditions sunny You picked housing Big You picked location Forest`;
-   
-    const actual = createCountString('sunny', 'Big', 'Forest');
-
-    expect.equal(actual, string);
-});
